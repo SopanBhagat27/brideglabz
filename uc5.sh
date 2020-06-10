a71f0aecca4b7d133e805e8dc28991c4dd08b405
Sopan <sopan280@gmail.com>
2020-06-10 09:15:07 +0530

    uc3 code

---
+++ uc5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ispart=1
+isfull=2
+perhrrate=20
+salary=0
+fullhr=8
+parthr=4
+days=20
+randomcheck=$((RANDOM%3))
+
+if [ $isfull -eq $randomcheck ]
+    then
+	salary=$(( $fullhr*$days*$perhrrate ))
+	echo "fill time salary for month " $salary 
+elif [ $ispart -eq $randomcheck ]
+    then
+	salary=$(( $parthr*$days*$perhrrate ))
+	echo "part time salary for month " $salary
+else
+	hrs=0
+fi
